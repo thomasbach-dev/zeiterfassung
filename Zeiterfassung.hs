833f11a1808fdaf2b8f392935291b9cb00c41f97
Thomas Bach <hacking@babibo.de>
2020-08-01 09:32:45 +0200

    Parsing log lines.

--- Zeiterfassung.hs
+++ Zeiterfassung.hs
@@ -6,10 +6,33 @@ import Text.Parsec.Text (Parser)
 
 import qualified Text.Parsec as P
 
-pWeekday :: Parser String
-pWeekday = P.choice $
-  map P.string [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
-               , "Saturday", "Sunday"]
+data LogLine = LogLine { startTime :: Time
+                       , endTime :: Time
+                       , subject :: String
+                       }
+             deriving (Eq, Show)
+
+data Time = Time Int Int
+          deriving (Eq, Show)
+
+pLogLine :: Parser LogLine
+pLogLine = do _ <- P.spaces *> P.anyChar `P.manyTill` P.space *> P.spaces
+              start <- pTime
+              _ <- P.char '-'
+              end <- pTime
+              _ <- P.space *> P.string "Clocked:" *> P.spaces
+                   *> P.char '(' *> pTime *> P.char ')' *> P.spaces
+              _ <- P.optional (pTaskState *> P.space)
+              subj <- P.anyChar `P.manyTill` P.newline
+              return (LogLine start end subj)
+
+pTime :: Parser Time
+pTime = do hour <- read <$> P.digit `P.manyTill` P.char ':'
+           minute <- read <$> P.count 2 P.digit
+           return (Time hour minute)
+
+pTaskState :: Parser String
+pTaskState = P.choice $ map P.string ["TODO", "DONE"]
 
 pDate :: Parser Day
 pDate = do _ <- pWeekday
@@ -34,3 +57,8 @@ pMonth =
 
 pYear :: Parser Integer
 pYear = read <$> P.count 4 P.digit
+
+pWeekday :: Parser String
+pWeekday = P.choice $
+  map P.string [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+               , "Saturday", "Sunday"]
--- tests/ZeiterfassungSpec.hs
+++ tests/ZeiterfassungSpec.hs
@@ -15,3 +15,10 @@ spec = do
       P.parse pDate "" "Tuesday    28 July 2020\n" `shouldBe` Right (fromGregorian 2020 7 28)
     it "Also parses the week number" $
       P.parse pDate "" "Monday     27 July 2020 W31\n" `shouldBe` Right (fromGregorian 2020 7 27)
+  describe "pLogLine" $ do
+    it "Parses a log line" $
+      P.parse pLogLine "" "  gtd02:      10:20-10:30 Clocked:   (0:10) Selbstorganisation\n"
+        `shouldBe` Right (LogLine (Time 10 20) (Time 10 30) "Selbstorganisation")
+    it "Skips the task state" $
+      P.parse pLogLine "" "  gtd02:      10:50-12:55 Clocked:   (2:05) TODO [INU-2697] CR204/SOAP-3: Soap - Light2Full\n"
+        `shouldBe` Right (LogLine (Time 10 50) (Time 12 55) "[INU-2697] CR204/SOAP-3: Soap - Light2Full")
