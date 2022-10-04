764fdcbf8919dafb9f0dba2b6efbbbb037c3954c
Thomas Bach <info@thomasbach.dev>
2022-10-04 07:28:30 +0200

    feat: fetch config from environemnt variable

--- Main.hs
+++ Main.hs
@@ -1,14 +1,22 @@
 module Main where
 
-import System.Environment (getArgs)
+import Data.List          (intercalate)
+import System.Environment (getArgs, lookupEnv)
 import System.Exit        (die)
 
 import Zeiterfassung
 
 main :: IO ()
 main = do
+  envVar <- lookupEnv "UNV_ZEITERFASSUNG_CFG"
   progArgs <- getArgs
-  case progArgs of
-    [cfgFile] -> readAndTransform cfgFile
-    _         -> die "Please provide exactlay one argument with the path to the configuration file"
+  cfgFile <- case (progArgs, envVar) of
+               ([cfgFile], _)    -> pure cfgFile
+               (_, Just cfgFile) -> pure cfgFile
+               _                 -> die . intercalate " " $ helpText
+  readAndTransform cfgFile
+  where
+    helpText = [ "Please provide either exactlay one argument with the path to the configuration file"
+               , "or set the UNV_ZEITERFASSUNG_CFG variable."
+               ]
 
