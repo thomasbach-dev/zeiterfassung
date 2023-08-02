696fda5ac15cd1efaf45d59fd811a27e2bece67c
Thomas Bach <info@thomasbach.dev>
2023-08-02 07:25:57 +0200

    build: added flake in favour of shell.nix and default.nix
    This also consistently renames the project from 'zeiterfassung' to 'unv-zeiterfassung'.

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# Taken from https://github.com/NixOS/templates/tree/master/haskell-hello
+{
+  description = "unv-zeiterfassung";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+      ghcVersion = "ghc92";
+    in
+    {
+      overlay = (final: prev: {
+        unv-zeiterfassung = final.pkgs.haskell.packages."${ghcVersion}".callCabal2nix
+          "unv-zeiterfassung"
+          ./.
+          {};
+      });
+      packages = forAllSystems (system: {
+         unv-zeiterfassung = nixpkgsFor.${system}.unv-zeiterfassung;
+      });
+      defaultPackage = forAllSystems (system: self.packages.${system}.unv-zeiterfassung);
+      checks = self.packages;
+      devShell = forAllSystems (system:
+        let
+          haskellPackages = nixpkgsFor.${system}.pkgs.haskell.packages."${ghcVersion}";
+        in haskellPackages.shellFor {
+          packages = p: with self.packages.${system}; [
+            # List all end leaf packages here
+            unv-zeiterfassung
+          ];
+          withHoogle = false;
+          buildInputs = with haskellPackages; [
+            cabal-install
+            ormolu
+          ];
+        # Change the prompt to show that you are in a devShell
+        shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+        });
+  };
+}
